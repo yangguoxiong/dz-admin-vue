c457321eb49afd6dd5ad70d326145a00b4fd161b
yangguoxiong <yangguoxiong@duizhuang.com>
2020-09-15 17:49:37 +0800

    首页完善

--- dz-vue/src/views/dashboard/index.vue
+++ dz-vue/src/views/dashboard/index.vue
@@ -1,14 +1,68 @@
 <template>
-  <div class="dashboard-container">
-    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank"><img style="margin: 40px"
-      src="http://img.heeexy.com/github.png"></a>
+  <div class="app-container">
+    <div v-if="user">
+      <el-row :gutter="20">
+
+        <el-col :span="6" :xs="24">
+          <user-card :user="user" />
+        </el-col>
+
+        <el-col :span="18" :xs="24">
+          <el-card>
+            <el-tabs v-model="activeTab">
+              <el-tab-pane label="Activity" name="activity">
+                <activity />
+              </el-tab-pane>
+              <el-tab-pane label="Timeline" name="timeline">
+                <timeline />
+              </el-tab-pane>
+              <el-tab-pane label="Account" name="account">
+                <account :user="user" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-card>
+        </el-col>
+
+      </el-row>
+    </div>
   </div>
 </template>
+
 <script>
+  import { mapGetters } from 'vuex'
+  import UserCard from './components/UserCard'
+  import Activity from './components/Activity'
+  import Timeline from './components/Timeline'
+  import Account from './components/Account'
+
   export default {
     name: 'dashboard',
+    components: { UserCard, Activity, Timeline, Account },
     data() {
-      return {}
+      return {
+        user: {},
+        activeTab: 'activity'
+      }
     },
+    computed: {
+      ...mapGetters([
+        'name',
+        'avatar',
+        'roles'
+      ])
+    },
+    created() {
+      this.getUser()
+    },
+    methods: {
+      getUser() {
+        this.user = {
+          name: 'admin',
+          role: 'admin',
+          email: 'admin@test.com',
+          avatar: '111'
+        }
+      }
+    }
   }
 </script>
